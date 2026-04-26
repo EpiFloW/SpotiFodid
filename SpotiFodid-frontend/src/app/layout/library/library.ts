55575caecd1ab95fcbd614959011a8971c070292
flowstoo <maisonnave.florian@gmail.com>
2026-04-27 00:27:44 +0200

    small library funct

--- SpotiFodid-frontend/src/app/layout/library/library.ts
+++ SpotiFodid-frontend/src/app/layout/library/library.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SmallSongCard } from '../../shared/small-song-card/small-song-card';
+import { SongService } from '../../service/song';
+import { ReadSong } from '../../service/model/song.model';
 
 @Component({
   selector: 'app-library',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterModule, SmallSongCard],
   templateUrl: './library.html',
   styleUrl: './library.scss',
 })
-export class Library {}
+
+export class Library implements OnInit {
+
+  private songService = inject(SongService);
+
+  songs: Array<ReadSong> = [];
+
+  isLoading = false;
+
+  constructor() {
+    effect(() => {
+      if(this.songService.getAllSig().status === "OK") {
+        this.songs = this.songService.getAllSig().value!;
+      }
+      this.isLoading = false;
+    });
+  }
+
+  ngOnInit(): void {
+    this.fetchSongs();
+  }
+
+  private fetchSongs() {
+    this.isLoading = true;
+    this.songService.getAll();
+  }
+}
